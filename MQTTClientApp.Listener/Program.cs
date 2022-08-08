024b75e3f3394c60e3f4f8e48f94d0c9c84206d2
Ahmed HM <ahmed.mohammed@kfactory.eu>
2022-08-08 19:03:32 +0100

    Publish and receive message

--- MQTTClientApp.Listener/Program.cs
+++ MQTTClientApp.Listener/Program.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System;
+using System.Text;
 
 namespace MQTTClientApp.Listener
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+       public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Listener starting ...");
+
+            var mqttFactory = new MqttFactory();
+
+            using (var mqttClient = mqttFactory.CreateMqttClient())
+            {
+                var mqttClientOptions = new MqttClientOptionsBuilder()
+                    .WithClientId("ListenerClient")
+                    .WithTcpServer("localhost", 1883)
+                    .Build();
+
+                // Setup message handling before connecting so that queued messages
+                // are also handled properly. When there is no event handler attached all
+                // received messages get lost.
+                mqttClient.ApplicationMessageReceivedAsync += e =>
+                {
+                    Console.WriteLine("Received application message : " + Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
+
+                    return Task.CompletedTask;
+                };
+
+                await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+                var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+                    .WithTopicFilter(f =>
+                    { 
+                        f.WithTopic("tests/console"); 
+
+                    }).Build();
+
+                await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+
+                Console.WriteLine("MQTT client subscribed to topic.");
+
+                Console.WriteLine("Press enter to exit.");
+                Console.ReadLine();
+            }
+
         }
+
+       
     }
 }
--- MQTTClientApp.Publisher/Program.cs
+++ MQTTClientApp.Publisher/Program.cs
@@ -1,12 +1,75 @@
-﻿using System;
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System;
 
 namespace MQTTClientApp.Publisher 
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Publisher starting ...");
+
+            var mqttFactory = new MqttFactory();
+
+            using (var mqttClient = mqttFactory.CreateMqttClient())
+            {
+                var options = new MqttClientOptionsBuilder()
+                   .WithClientId("PublisherClient")
+                   .WithTcpServer("localhost", 1883)
+                   .WithCleanSession()
+                   .Build();
+
+                mqttClient.ConnectedAsync += (s) =>
+                {
+                    Console.WriteLine("Publisher connected !");
+
+                    return Task.CompletedTask;
+                };
+
+                mqttClient.DisconnectedAsync += (s) =>
+                {
+                    Console.WriteLine("Publisher disconnected !");
+
+                    return Task.CompletedTask;
+                };
+
+                await mqttClient.ConnectAsync(options, CancellationToken.None);
+
+                Console.WriteLine("Press any key to publish a message");
+
+                Console.ReadLine();
+
+                PublishMessage(mqttClient, "Salam");
+
+                Console.ReadLine();
+            }
+
         }
+
+        private static void PublishMessage(IMqttClient client ,string messageText)
+        {
+            var message = new MqttApplicationMessageBuilder()
+            .WithTopic("tests/console")
+            .WithPayload(messageText)
+            .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
+            .Build();
+
+            if(client.IsConnected)
+            {
+                client.PublishAsync(message, CancellationToken.None);
+
+                Console.WriteLine("Message published");
+            }
+            else
+            {
+                Console.WriteLine("Message not published : client is disconnected!");
+            }
+        }
+
+
+
+
     }
 }
