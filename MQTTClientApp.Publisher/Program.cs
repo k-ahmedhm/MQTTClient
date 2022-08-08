845d834e150f28b581be414ed21bf4628bc16d51
Ahmed HM <hm.Ahmed@fikrasystems.com>
2022-08-08 15:16:00 +0100

    Create solution files

---
+++ MQTTClientApp.Publisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MQTTClientApp.Publisher 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
